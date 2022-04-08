79cee1ec78a04b1d9c603f2f884bff47e9cefc53
noorj <noorj.in@mouritech.com>
2022-04-08 16:36:53 +0530

    initial Commit

---
+++ lib/constraints/colors.dart
@@ -0,0 +1,20 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+final customColor = CustomColors();
+class CustomColors{
+  final Color blue = const Color(0xFF1E1E51);
+  final Color blueLight = const Color(0xFF292961);
+  final Color blueAccent = const Color(0xFF5B8EC7);
+  final Color yellow = const Color(0xFFF9D84B);
+  final Color skyBlue = const Color(0xFF56BDEB);
+  final Color skyBlueAccent = const Color(0xFFA2DAF3);
+  final Color green = const Color(0xFF12D069);
+  final Color grey = const Color(0xFFE7E7E7);
+  final Color white = Colors.white;
+  final Color black = Colors.black;
+  final Color gray = const Color(0xFF848484);
+  final Color lightGray = const Color(0xFFECECEC);
+  final Color black33 = const Color(0xFF333333);
+  final Color labelColor = const Color(0xFF6F6F6F);
+  final Color cyan = const Color(0xFF26C6DA);
+}
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_todo_app/views/login_page.dart';
+
+void main() async{
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+      overlays: [SystemUiOverlay.bottom, SystemUiOverlay.top]);
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const LoginPage(),
+    );
+  }
+}
+
